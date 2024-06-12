e4df53ce52f224cf9a62c6b5bb5be7b8a57912ed
bernard411 <hamiebernard@gmail.com>
2024-06-12 09:32:50 -0500

    updates

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+
+    path('', views.homepage, name="homepage"),
+    path('text/recogi/', views.output_text, name="output")
+   
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
---
+++ core/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from .forms import ImageUploadForm
+from .models import ImageData
+from PIL import Image
+import pytesseract
+
+def homepage(request):
+
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            img = form.cleaned_data['image']
+            img_obj = Image.open(img)  
+            img_obj = img_obj.convert('L')
+            text = pytesseract.image_to_string(img_obj)
+            imagedata = ImageData(image=img) 
+            imagedata.save() 
+            return render(request, 'index.html', {'text': text, 'image': imagedata})
+    else:
+        form = ImageUploadForm()
+ 
+    imagedata= ImageData.objects.all()
+    context = {'form': form, 'imagedata': imagedata}
+    return render(request, 'index.html', context)
+
+def output_text(request):
+
+
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            img = form.cleaned_data['image']
+            img_obj = Image.open(img)  
+            img_obj = img_obj.convert('L')
+            text = pytesseract.image_to_string(img_obj)
+            imagedata = ImageData(image=img) 
+            imagedata.save() 
+            return render(request, 'index.html', {'text': text, 'image': imagedata})
+    else:
+        form = ImageUploadForm()
+ 
+    imagedata= ImageData.objects.all()
+    context = {'form': form, 'imagedata': imagedata}
+    return render(request, 'output.html', context)
